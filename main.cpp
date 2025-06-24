c65957b52b503f2107df51bd4e25d1ba2b378420
ibrahemnaser <i.naser4040@gmail.com>
2025-06-24 20:55:03 +0300

    [Init]: clear unused code and init for revising pointers

--- main.cpp
+++ main.cpp
@@ -1,47 +1,10 @@
 #include <iostream>
-#include "Student.h"
 using namespace std;
-
-// Student struct with declaration and implementation
-// struct Student
-// {
-//   string name;
-//   Student(string _name)
-//   {
-//     name = _name;
-//   }
-
-//   void print()
-//   {
-//     cout << "Student name is: " << name << endl;
-//   }
-// };
-
-// How to separate declaration and implementation
-// [1] Declaration
-// struct Student
-// {
-//   string name;
-
-//   Student(string _name);
-//   void print();
-// };
-
-// // [2] Implementation
-// Student::Student(string _name)
-// {
-//   name = _name;
-// }
-
-// void Student::print()
-// {
-//   cout << "Student name is: " << name << endl;
-// }
 
 int main()
 {
-  Student s1("Medhat");
-  s1.print();
+
+  cout << "Hello World" << endl;
 
   return 0;
 }
