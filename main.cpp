846f3bb8221aff585fd7b60d7c304635ec3ae68d
ibrahemnaser <i.naser4040@gmail.com>
2025-07-13 08:42:51 +0300

    [Update]: learn about construction and destruction for a class in addition to handling memory leaks resulting from using pointers inside class

--- main.cpp
+++ main.cpp
@@ -1,12 +1,71 @@
 #include <iostream>
 using namespace std;
 
+class Ty
+{
+
+private:
+  int x;
+  int *y;
+
+public:
+  Ty() : Ty(0, 0)
+  {
+    cout << "Default Constructor" << endl;
+  }
+  Ty(int x, int _y) : x(x)
+  {
+    cout << "Constructor" << endl;
+    y = new int;
+    *y = _y;
+  }
+  ~Ty()
+  {
+    cout << "Destructor" << endl;
+    delete y; // memory leak
+  }
+
+  void setX(int x)
+  {
+    this->x = x;
+  }
+
+  int getX()
+  {
+    return x;
+  }
+
+  int getY()
+  {
+    return *y;
+  }
+
+  void setY(int _y)
+  {
+    *y = _y;
+  }
+};
+
 int main()
 {
 
   cout << "***********************************" << endl;
   cout << "************ OOP WORLD ************" << endl;
   cout << "***********************************" << endl;
+
+  Ty t1(100, 88);
+  cout << "x = " << t1.getX() << endl;
+
+  Ty t2;
+  t2.setX(10);
+  t2.setY(20);
+  cout << "x = " << t2.getX() << endl;
+  cout << "y = " << t2.getY() << endl;
+
+  Ty *t3 = new Ty(23, 21);
+  cout << "x = " << t3->getX() << endl;
+
+  delete t3; // memory leak
 
   return 0;
 }
